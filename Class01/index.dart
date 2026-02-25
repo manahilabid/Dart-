4f7aaa1aa11733d4935de897cd58bf05b01feecc
manahilabid <soniaabid806@gmail.com>
2026-02-24 17:11:18 -0800

    varibles

---
+++ Class01/index.dart
@@ -0,0 +1,26 @@
+void main(){
+  print("Hello World");
+  String name ="user";
+  print(name);
+  int number = 12;
+  print(number);
+  double decimal = 12.3;
+  print(decimal);
+  bool work = true;
+  print(work);
+  
+   int num1 = 21;
+   int num2 = 21;
+  int result = num1+num2;
+  print(result);
+   
+  if(result == 42){
+    print("Correct");
+
+  }
+  else{
+print("Incorrect");
+  }
+
+
+}
